b1046e0e93fe93fc06c61be086cc683ac47e4015
jamescoder92 <kusimbajames92@gmail.com>
2026-04-16 13:10:07 +0300

    js-analysis

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const { format } = require('date-fns');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
 
+  for (const arr of args) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
 
+  combinedObject.merge_date = format(new Date(), 'M/d/yyyy');
+
+  return combinedObject;
+}
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
